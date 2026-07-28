58193661bb2d91c811c86f03a249dd5011e673df
Deepika <deepika200110@gmail.com>
2026-07-28 21:14:50 +0530

    Added Amudham media folder and logo assets

--- obj/Debug/net8.0/AmmasKitchen.AssemblyInfo.cs
+++ obj/Debug/net8.0/AmmasKitchen.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmmasKitchen")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c682abd523ee2a51f9d7cfc76dfee0cf75fff51")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmmasKitchen")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmmasKitchen")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
